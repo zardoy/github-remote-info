79516f2fb088439d411710858de1d1e42529a235
Vitaly Turovsky <vital2580@icloud.com>
2021-09-07 07:06:11 +0300

    run prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { URL } from 'url';
-import remoteOrigin from 'git-remote-origin-url';
+import { URL } from 'url'
+import remoteOrigin from 'git-remote-origin-url'
 
 //TODO didn't name repo because it could be annoying to see in import suggestions
 export interface RepoInfo {
@@ -8,30 +8,28 @@ export interface RepoInfo {
 }
 
 export const getGithubRemoteInfo = async (repoRootPath: string): Promise<RepoInfo | undefined> => {
-    let originUrl: undefined | string;
+    let originUrl: undefined | string
     try {
         try {
-            originUrl = await remoteOrigin(repoRootPath);
+            originUrl = await remoteOrigin(repoRootPath)
         } catch (error) {
-            if (error.message.startsWith('Couldn\'t find')) originUrl = undefined;
-            else throw error;
+            if (error.message.startsWith("Couldn't find")) originUrl = undefined
+            else throw error
         }
 
-        if (!originUrl) return;
+        if (!originUrl) return
 
-        const gitMatch = /git@github.com:(?<owner>\w+)\/(?<name>.+)(.git)/.exec(originUrl);
-        if (gitMatch)
-            return gitMatch.groups! as any;
+        const gitMatch = /git@github.com:(?<owner>\w+)\/(?<name>.+)(.git)/.exec(originUrl)
+        if (gitMatch) return gitMatch.groups! as any
 
-        const url = new URL(originUrl);
-        if (url.hostname !== 'github.com') throw new Error(`Unknown host ${url.hostname}`);
+        const url = new URL(originUrl)
+        if (url.hostname !== 'github.com') throw new Error(`Unknown host ${url.hostname}`)
 
-        let [, owner, name] = url.pathname.split('/');
-        if (name.endsWith('.git'))
-            name = name.slice(0, -'.git'.length);
+        let [, owner, name] = url.pathname.split('/')
+        if (name.endsWith('.git')) name = name.slice(0, -'.git'.length)
 
-        return { owner, name };
+        return { owner, name }
     } catch (error) {
-        throw new Error(`${error.message} Error occured in ${repoRootPath} with remote origin ${originUrl!}`);
+        throw new Error(`${error.message} Error occured in ${repoRootPath} with remote origin ${originUrl!}`)
     }
-};
+}
